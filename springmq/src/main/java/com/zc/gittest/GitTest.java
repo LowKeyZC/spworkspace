28cc1219d8fef76f21249e8180371ac7707d0bc6
zhouchao <zhouchao2@100tal.com>
2018-04-24 13:28:50 +0800

    git add test

--- springmq/src/main/java/com/zc/gittest/GitTest.java
+++ springmq/src/main/java/com/zc/gittest/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test change git");
     }
+
+    public static void abcd(){
+        System.out.println("conflict test");
+    }
 }
