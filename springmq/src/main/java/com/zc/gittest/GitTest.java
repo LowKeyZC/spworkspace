96fb0bb34de56a7bb00430b4ebc584b458629cd8
zhouchao <zhouchao2@100tal.com>
2018-04-24 11:05:54 +0800

    git add test

--- springmq/src/main/java/com/zc/gittest/GitTest.java
+++ springmq/src/main/java/com/zc/gittest/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test change git");
     }
+    public static void newBranch(String[] args) {
+        System.out.println("new branch test");
+    }
 }
